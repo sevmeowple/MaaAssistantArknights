901d872ecd50c277bc2af27db6a5f92cc7cdc843
Weiyou Wang <blspgzs@gmail.com>
2024-07-22 04:37:19 +1000

    feat: 为肉鸽插件加入联动功能，仅在必要时使用

--- src/MaaCore/Task/Roguelike/AbstractRoguelikeTaskPlugin.h
+++ src/MaaCore/Task/Roguelike/AbstractRoguelikeTaskPlugin.h
@@ -13,7 +13,16 @@ namespace asst
         AbstractRoguelikeTaskPlugin(const AsstCallback& callback, Assistant* inst, std::string_view task_chain,
                                     std::shared_ptr<RoguelikeConfig> data);
 
+        virtual std::optional<json::value> response_to_event(
+            [[maybe_unused]] RoguelikeEvent event,
+            [[maybe_unused]] const json::value& detail)
+        {
+            return std::nullopt;
+        };
+
     protected:
         std::shared_ptr<RoguelikeConfig> m_config;
+        std::optional<json::value>
+            notify_sibling_plugins(RoguelikeEvent event, const json::value& detail = json::object());
     };
 }
